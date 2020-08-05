1d6baf759243239f242c1acb38c2fc71bc41851c
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-05 14:48:29 +0000

    Ajout d'un middleware à mongoose pour l'authentification d'utilisateur

---
+++ src/middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+
+const User = require('../models/user');
+
+
+const auth = async (req, res, next) => {
+    try {
+        // Get token from request
+        /* here replace() will remove the 'Bearer ' placed in front of the token
+            If 'Bearer ' isn't found, it will throw an error that will be caught by the catch */
+        const token = req.header('Authorization').replace('Bearer ', '');
+        // here I use as second argument the secret word used in /models/user.js to build the token
+        const decoded = jwt.verify(token, 'thisismysecretwordfortoken');
+        // find the user with the correct _id and check if given token exists in user.tokens
+        const user = await User.findOne({ _id: decoded._id, 'tokens.token': token });
+
+        if (!user) {
+            throw new Error();
+        }
+
+        // add token to req so we can delete it easily if user is logging out
+        req.token = token;
+        // add the found user in the request
+        req.user = user;
+
+        // Everything is fine, call the async function given in the route
+        next();
+    } catch {
+        res.status(401).send('Erreur: Vous devez d\'abord vous identifier.');
+    }
+}
+
+module.exports = auth;
