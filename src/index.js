c12402de54acd91ec4393922c547561021dfcf87
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-05 12:49:57 +0000

    implémentation templating avec handlebars, création rapide page 404 pour tests

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
+/* NodeJS code modules */
+const path = require('path');
+
+/* npm modules */
 const express = require('express');
+// hbs module so we can use partial templates
+const hbs = require('hbs');
 
 // call mongoose.js, it will launch the file, connect to db and create database if doesn't exist
 require('./db/mongoose');
@@ -17,6 +23,25 @@ const hostname = '192.168.0.50';
 // used port
 const port = 3002;
 
+/* Define paths for express config */
+// build the public path from absolute path
+const publicDirectoryPath = path.join(__dirname, '../public');
+// views directory
+const viewsPath = path.join(__dirname, '../templates/views');
+// partials templates location
+const partialPath = path.join(__dirname, '../templates/partials');
+
+
+/* Setup handlebars engine and views location */
+// Tell express we're gonna use hbs as a template engine
+app.set('view engine', 'hbs');
+// Tell express we have moved the views directory
+app.set('views', viewsPath);
+// Tell hbs we're gonna use some partial templates
+hbs.registerPartials(partialPath);
+
+
+
 // Tell express to parse json when we receice some (NEEDED for POST requests !)
 app.use(express.json());
 
@@ -29,6 +54,16 @@ app.get('', (req, res) => {
 // app.use(userRouter);
 // app.use(gridRouter);
 // app.use(optionsRouter);
+
+
+// 404 Error page, !! this must be the very last route
+app.get('*', (req, res) => {
+    res.render('404', {
+        title: 'ririgram',
+        author: 'Richard Meuret',
+        message404: 'Page non trouvée !'
+    });
+});
 
 
 // Launch server
