265c1f8872275452c5b791f21017d49d8bcebfd9
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-07 13:20:13 +0000

    ajout route Login User

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,9 +1,12 @@
 const express = require('express');
 
 const User = require('../models/user');
+const Options = require('../models/options');
 
 const auth = require('../middleware/auth');
-const { __esModule } = require('validator/lib/isAlpha');
+// const { __esModule } = require('validator/lib/isAlpha');   // ?????
+
+const defaultColors = require('../../defalut_values/colors');
 
 const router = new express.Router();
 
@@ -19,6 +22,28 @@ router.post('/users', async (req, res) => {
         const token = await user.generateAuthToken();
 
         res.status(201).send({ user, token });
+    } catch (e) {
+        res.status(400).send(e);
+        console.log(e);
+    }
+});
+
+
+// User login
+router.post('/users/login', async (req, res) => {
+    try {
+        // get user and give him a token
+        const user = await User.findByCredentials(req.body.email, req.body.password);
+        const token = await user.generateAuthToken();
+
+        // get user options
+        let options = await Options.findOne({ owner: user._id});
+        if (!options) {
+            options = defaultColors;
+        }
+
+        res.send({ user, token, options });
+
     } catch (e) {
         res.status(400).send(e);
         console.log(e);
