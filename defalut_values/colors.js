15605815f05acd64a5a1fb82582bab88b9bf94e3
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-07 13:14:49 +0000

    ajout fichier /default_values/colors.js et /src/models/options.js

---
+++ defalut_values/colors.js
@@ -0,0 +1,11 @@
+const defaultColors = {
+    colorHeadBackground: '#333333',
+    colorHeadFont: '#dddddd',
+    colorAnswerOrigin: '#dddddd',
+    colorAnswerNo: '#333333',
+    colorAnswerYes: '#FF8800',
+    colorAnswerMaybeNo: '#555555',
+    colorAnswerMaybeYes: '#777999'
+};
+
+module.exports = defaultColors;
---
+++ src/models/options.js
@@ -0,0 +1,76 @@
+const mongoose = require('mongoose');
+
+const regex = /^#[a-fA-F0-9]{6}/;
+
+
+const optionsSchema = new mongoose.Schema({
+    colors: {
+        colorHeadBackground: {
+            type: String,
+            required: [true, 'Il manque la couleur du background des indications.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorHeadFont: {
+            type: String,
+            required: [true, 'Il manque la couleur des indications.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorAnswerOrigin: {
+            type: String,
+            required: [true, 'Il manque la couleur des cases lorsque pas de réponse.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorAnswerNo: {
+            type: String,
+            required: [true, 'Il manque la couleur des cases lorsque réponse \'Non\'.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorAnswerYes: {
+            type: String,
+            required: [true, 'Il manque la couleur des cases lorsque réponse \'Oui\'.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorAnswerMaybeNo: {
+            type: String,
+            required: [true, 'Il manque la couleur des cases lorsque réponse \'Peut-être Non\'.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+        colorAnswerMaybeYes: {
+            type: String,
+            required: [true, 'Il manque la couleur des cases lorsque réponse \'Peut-être Oui\'.'],
+            trim: true,
+            validate: {
+                validator: (value) => { return !regex.test(value); }
+            }
+        },
+    },
+    Owner: {
+        type: mongoose.Schema.Types.ObjectId,
+        require: true,
+        ref: 'User'
+    }
+});
+
+
+const Options = mongoose.model('Options', optionsSchema);
+
+
+module.exports = Options;
