af16820c7b77bf6e638cc9c9909087c1510b57e3
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-12 22:17:46 +0000

    Ajout route /grid/test-play

---
+++ src/routers/grid.js
@@ -0,0 +1,22 @@
+const express = require('express');
+
+const computeGridSolution = require('../utils/compute-grid-solution');
+const computeHelpers = require('../utils/compute-helpers');
+
+const router = new express.Router();
+
+
+// User is not logged and wants to test the game
+router.post('/grid/test-game', async (req, res) => {
+    try {
+    // Compute a 4 rows 4 columns grid for the user
+    const { gridSolution, clicksNbForPerfectGame } = await computeGridSolution(rowsNb * colsNb);
+    // Get rows and cols helpers
+    const { rowsHelpers, maxRowHelpers, colsHelpers, maxColHelpers } = await computeHelpers(colsNb, gridSolution);
+
+    res.send({ gridSolution, clicksNbForPerfectGame, rowsHelpers, maxRowHelpers, colsHelpers, maxColHelpers });
+    }
+    catch (e) {
+        res.status(500).send('Quelque chose s\'est mal déroulé pendant la création de la grille.');
+    }
+});
