b6062afc0a564d611da53d826c3f88fc85d8559b
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-16 13:21:04 +0000

    Ajout des boutons de choix de couleur de réponse, html, scss et js

--- public/js/game-manager.js
+++ public/js/game-manager.js
@@ -8,13 +8,36 @@ const gameManager = (rowsNb, colsNb, rowsHelpers, maxRowHelpers, colsHelpers, ma
 
     // watch any window rezising
     watchWindowSize(rowsNb, colsNb, maxRowHelpers, maxColHelpers);
+
+    // react when user push a game button
+    watchGameButtons();
 };
 
 
-
+// When screeen size change
 const watchWindowSize = (rowsNb, colsNb, maxRowHelpers, maxColHelpers) => {
     window.addEventListener('resize', () => {
         setCssGridSize(rowsNb, colsNb, maxRowHelpers, maxColHelpers);
     });
+};
+
+
+// User is playing
+const watchGameButtons = () => {
+    const answerBtns = document.querySelectorAll('.answer-btn');
+
+    console.log(answerBtns)
+
+    answerBtns.forEach((answerBtn) => {
+
+        answerBtn.addEventListener('click', (event) => {
+
+            answerBtns.forEach((elem) => {
+                elem.classList.remove('current-choice');
+            });
+    
+            event.target.classList.add('current-choice');
+        });
+    });    
 };
 
