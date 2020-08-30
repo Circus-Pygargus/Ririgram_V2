5dfe99de78e5ffeaa877c5bb84cf5581d47e725d
Circus-Pygargus <richard.meuret@laposte.net>
2020-08-30 22:56:06 +0000

    Ajout en cours : route /grid/new

--- src/routers/grid.js
+++ src/routers/grid.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+const Grid = require('../models/grid');
+const UserTimeHard = require('../models/userTimeHard');
+
+const auth = require('../middleware/auth');
 const computeGridSolution = require('../utils/compute-grid-solution');
 const computeHelpers = require('../utils/compute-helpers');
 
@@ -24,4 +28,46 @@ router.post('/grid/test-game', async (req, res) => {
 });
 
 
+// User is loggued and asked to play a new game
+router.post('/grid/new', auth, async (req, res) => {
+    try {
+        const {rowsNb, colsNb} = req.body;
+        const gridsFound = await Grid.find({ rowsNb, colsNb });
+        console.log('gridsFound');
+        console.log(gridsFound);
+
+        let isnewGridFound = false;
+
+        gridsFound = gridsFound.filter((gridFound) => {
+            console.log('gridFound');
+            console.log(gridFound);
+            const userTimeHard = await UserTimeHard.findOne({ owner: req.user._id, grid: gridFound._id});
+            console.log('userTimeHard')
+            console.log(userTimeHard)
+            if (!userTimeHard) return true;
+            else return false;
+        });
+        // at least one grid not played by this user found in DB
+        if (gridsFound.length > 0) {
+            // enregistrer quelque part le chrono de départ !!
+        } 
+
+    } catch (e) {
+        console.log(e);
+        res.status(500).send('Un problème est survenu pendant la création de la grille.');
+    }
+});
+
+
 module.exports = router;
+
+/*
+router(grid, auth, (req, res) => {
+    si auth ok
+    recherche de grille en bdd non jouée par le joueur (ne pas oublier la difficulté)
+    si !grille -> création complète de grille
+        enregistrement grille en bdd
+        envoi de la grille
+    si grille -> envoi de la grille
+})
+*/
