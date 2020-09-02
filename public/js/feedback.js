1828df467b2eab14b07814a18c66d8a8fb7154e5
Circus-Pygargus <richard.meuret@laposte.net>
2020-09-02 23:11:12 +0000

    Ajout : afficher/cacher le formulaire d'envoi de feedback

---
+++ public/js/feedback.js
@@ -0,0 +1,13 @@
+const watchFeedback = () => {
+    
+    const wantGiveFeedbackBtn = document.querySelector('#want-give-feedback');
+    const feedbackForm = document.querySelector('#feedback-form');
+
+
+    wantGiveFeedbackBtn.addEventListener('click', (event) => {
+        feedbackForm.classList.remove('d-none');
+        feedbackForm.scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+};
