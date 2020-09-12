661c8c825af4141841990dea0954fb32407af159
Circus-Pygargus <richard.meuret@laposte.net>
2020-09-12 23:02:50 +0000

    Modif html concernant les infos de version

--- public/js/info.js
+++ public/js/info.js
@@ -1,13 +1,45 @@
 const watchInfos = () => {
-    const showNewInfoBtn = document.querySelector('#show-new-info-btn');    
-    const newInfoDiv = document.querySelector('#new-info-div');
+    // hide/show the form
+    const showNewInfoBtn = document.querySelector('#show-new-info-btn');
     const hideNewInfoBtn = document.querySelector('#hide-new-info-btn');
+    const newInfoDiv = document.querySelector('#new-info-div');
+    // form
+    const newInfoForm = document.querySelector('#new-info-form');
+    // form content
+    const newInfoVersionInput = document.querySelector('#new-info-version');
+    const newInfoTitleInput = document.querySelector('#new-info-title');
+    const newInfoMessageInput = document.querySelector('#new-info-message');
+
 
     showNewInfoBtn.addEventListener('click', () => {
         newInfoDiv.classList.remove('d-none');
     });
 
+
     hideNewInfoBtn.addEventListener('click', () => {
         newInfoDiv.classList.add('d-none');
+    });
+
+
+    newInfoForm.addEventListener('click', (e) => {
+        e.preventDefault();
+        const token = sessionStorage.getItem('token');
+        const data = {
+            "version": newInfoVersionInput.value,
+            "title": newInfoTitleInput.value,
+            "message": newInfoMessageInput.value
+        };
+        fetch('/infos/new', {
+            method: 'POST',
+            body: JSON.stringify(data),
+            headers: {
+                "Content-Type": "application:json",
+                "Authorization": `Bearer ${JSON.parse(token)}`
+            }
+        })
+        .then(response => response.json())
+        .then(response => {
+            if (response.error) sendNotification('error', response.error);
+        })
     });
 };
