a6851dd03adedad31fa9ccfc03e33a5a6028f5b8
Circus-Pygargus <richard.meuret@laposte.net>
2020-09-12 22:58:52 +0000

    Modif de la route /infos/new

--- src/routers/info.js
+++ src/routers/info.js
@@ -1,10 +1,14 @@
 const express = require('express');
 
+const path =require('path');
+
 const Info = require('../models/info');
 
 const auth = require('../middleware/auth');
 
 const router = new express.Router();
+
+const partialsPath = path.join(__dirname, '../../templates/partials');
 
 
 // Admin wants to record a new info
@@ -13,7 +17,23 @@ router.post('/infos/new', auth, async (req, res) => {
         const { version, title, message } = req.body;
         const info = new Info({ version, title, message });
         await info.save();
-        res.status(201).send({ info });
+        let infos = await Info.find({});
+        // remove message from all infos
+        for (let i = 0, max = infos.length; i < max; i++) {
+            let infoObject = infos[i].toObject();
+            delete infoObject.message;
+            infos[i] = new Info(infoObject);
+        }
+        res.render(`${partialsPath}/infos`, { infos }, (err, html) => {
+            if (err) {
+                return res.send({
+                    error: 'Une erreur est survenue pendant le rendu des infos'
+                });
+            }
+
+            res.status(201).send({ html });
+        });
+        
     } catch(e) {
         console.log(e);
         res.status(500).send({ error: e.message });
